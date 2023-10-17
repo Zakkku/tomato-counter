bdf75585cb5514f4292953bdd48430f280e28d80
Zach <wakuwakuzakku@gmail.com>
2023-10-17 17:14:11 +0900

    created tomatoes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [tomatoes, setTomatoes] = useState(0);
+
+  const tomatoImages = [];
+
+  for (let i = 0; i < tomatoes; i++) {
+    tomatoImages.push('🍅');
+  };
+
+  const tomatoString = tomatoImages.join('');
+
+
   return (
     <div className="App">
       <div className='window'>
         <h2>Tomato Counter</h2>
         <h4>A simple React counter app with useState</h4>
         <div className='tomato-box'>
-          <h1>0</h1>
+          <h1>
+            {tomatoString}
+          </h1>
         </div>
         <div className='buttons-container'>
-          <div className='button'>-</div>
-          <div className='button'>+</div>
+          <div 
+            className='button'
+            onClick={() => setTomatoes(tomatoes - 1)}
+            >
+              -
+          </div>
+          <div 
+          className='button'
+          onClick={() => setTomatoes(tomatoes + 1)}
+          >
+            +
+          </div>
         </div>
       </div>
     </div>
