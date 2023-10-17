2df3c5dcef2e6bb25b59dfb2cd933ebb197f3ee6
Zach <wakuwakuzakku@gmail.com>
2023-10-17 16:11:29 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='window'>
+        <h2>Tomato Counter App</h2>
+        <div className='tomato-box'>
+          <h1>0</h1>
+        </div>
+        <div className='buttons-container'>
+          <div className='button'>-</div>
+          <div className='button'>+</div>
+        </div>
+      </div>
     </div>
   );
 }
